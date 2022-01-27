280c2a081793e3a9744520d788d1efb02c463911
mateo <Mrblaszka>
2022-01-27 13:49:44 +0100

    Dwie funkcje

--- app/src/main/java/com/example/lista/MainActivity.kt
+++ app/src/main/java/com/example/lista/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CheckedTextView
 import android.widget.Toast
-import com.example.Lista.databinding.ActivityMainBinding
+import com.example.lista.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,10 +16,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         setupCheckedTextView()
-        CheckedTextView2()
+        setupCheckedTextView3()
     }
     private fun setupCheckedTextView() {
-        val checkedTextView2 = binding.checkedTextView
+        val checkedTextView2 = binding.checkedTextView2
         checkedTextView2.isChecked = false
         checkedTextView2.setCheckMarkDrawable(android.R.drawable.checkbox_off_background)
         checkedTextView2.setOnClickListener {
@@ -37,6 +37,29 @@ class MainActivity : AppCompatActivity() {
                     else
                         R.string.unchecked)
             Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun setupCheckedTextView3() {
+        val checkedTextView3 = binding.checkedTextView3
+        checkedTextView3.isChecked = false
+        checkedTextView3.setCheckMarkDrawable(android.R.drawable.checkbox_off_background)
+
+        checkedTextView3.setOnClickListener {
+            checkedTextView3.isChecked = !checkedTextView3.isChecked
+            checkedTextView3.setCheckMarkDrawable(
+                if (checkedTextView3.isChecked)
+                    android.R.drawable.checkbox_off_background
+                else
+                    android.R.drawable.checkbox_on_background)
+
+            val msg2 =
+                getString(R.string.pre_msg2) + " " + getString(
+                    if (checkedTextView3.isChecked)
+                        R.string.checked
+                    else
+                        R.string.unchecked)
+
+            Toast.makeText(this@MainActivity, msg2,Toast.LENGTH_SHORT).show()
         }
     }
 }
